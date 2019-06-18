83a8bd0a7cd1c2c8eab43508cdcdc222a872acd7
Akil Aikman <soulchips@Soul-MacBook-Pro.local>
2019-06-18 12:22:28 -0400

    updated fetch command

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ const message2 = document.querySelector('#message-2')
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()
 
-  fetch(`http://localhost:3000/weather?address=${search.value}`).then((response) => {
+  fetch(`/weather?address=${search.value}`).then((response) => {
     response.json().then((data) => {
       if(data.error) {
         message1.textContent = data.error
